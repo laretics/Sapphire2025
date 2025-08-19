fdf6cc4a3704e9a9a0cf08a6f5b67167a55053b8
laretics <spammer@montefaro.eu>
2025-08-19 13:18:13 +0200

    Otra vez, contenidos de los turnos de trabajo accesibles desde el objeto del modelo, para poder usarlos en el lado del cliente Blazor.

--- Sapphire2025Models/Expert/WorkShiftTemplateCollectionModel.cs
+++ Sapphire2025Models/Expert/WorkShiftTemplateCollectionModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Sapphire2025Models.Expert
@@ -18,6 +19,7 @@ namespace Sapphire2025Models.Expert
 		public byte Collective { get; set; }
 		public Guid Owner { get; set; }		
 		public List<WorkShiftTemplateModel>? Templates { get; set; } //Lista completa de plantillas que contiene este plan.
+
 		/// <summary>
 		/// Devuelve el turno correspondiente a este día en base a la palabra clave con el que se invoca.
 		/// </summary>
@@ -78,7 +80,11 @@ namespace Sapphire2025Models.Expert
 
             return adding;
 		}
-	
+
+
+		[JsonIgnore] //Caché con los contenidos del último turno solicitado.
+		protected List<WorkShiftContentModel> mcolContents = new List<WorkShiftContentModel>();
+
 		/// <summary>
 		/// Obtiene todos los templates vigentes un día concreto del año.
 		/// Se usa para la vista diaria de turnos cubiertos.
@@ -89,15 +95,38 @@ namespace Sapphire2025Models.Expert
 		public List<WorkShiftTemplateModel> TemplatesByDay (DateTime day, bool festive)
 		{
 			List<WorkShiftTemplateModel> salida = new List<WorkShiftTemplateModel>();
+			mcolContents = new List<WorkShiftContentModel>(); //Inicio caché de contenidos.
 			if(null!=Templates)
 			{
                 foreach (WorkShiftTemplateModel auxTemplate in Templates)
                 {
                     if (auxTemplate.IsEnabled(day.DayOfWeek, festive))
-                        salida.Add(auxTemplate);
+					{
+						salida.Add(auxTemplate);
+						if(auxTemplate is AttTemplateModel auxContentProvider)
+						{
+							if(null!=auxContentProvider.Content)
+							{
+								foreach (WorkShiftContentModel contenido in auxContentProvider.Content)
+								{
+									contenido.Parent = auxContentProvider;
+									mcolContents.Add(contenido);
+								}
+							}
+						}
+					}                        
                 }
             }
 			return salida;
+		}
+
+		/// <summary>
+		/// Obtiene los contenidos de la última colección de turnos solicitada con la función anterior. TemplatesByDay.
+		/// </summary>
+		/// <returns>Colección de contenidos.</returns>
+		public List<WorkShiftContentModel> ContentsByLastDay ()
+		{
+			return mcolContents;
 		}
 	}
 }
