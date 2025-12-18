d9db992e495e73143c65b31f5a0b648b5eecb2c2
laretics <spammer@montefaro.eu>
2025-12-18 20:33:51 +0100

    Asimilation

--- Sapphire25/TimeNet2026/DBStorage/OnyxDatabase.cs
+++ Sapphire25/TimeNet2026/DBStorage/OnyxDatabase.cs
@@ -127,6 +127,9 @@ namespace TimeNet2026.DBStorage
 			List<Axis> auxEjes = await getAxis();
 			foreach(Axis eje in auxEjes)
 				salida.mcolAxis.Add(eje.id, eje);
+			List<Asimilation> asimilations = await GetAsimilations();
+			foreach (Asimilation asim in asimilations)
+				salida.mcolAsimilations.Add(asim.id, asim);			
 
 			return salida;
 		}
@@ -146,7 +149,9 @@ namespace TimeNet2026.DBStorage
 			//Ahora metemos todos los ejes
 			foreach (Axis eje in rhs.ColAxis)
 				await Insert(eje);
-
+			//Metemos las asimilaciones que contiene
+			foreach (Asimilation asim in rhs.ColAsimilations)
+				await Insert(asim);
 		}
 		internal async Task RemoveTopoStorage(Guid rhs, bool update = true)
 		{
@@ -260,6 +265,22 @@ namespace TimeNet2026.DBStorage
 			return null;
 		}
 
+		internal async Task<List<Asimilation>> GetAsimilations()
+		{
+			List<Asimilation> salida = new List<Asimilation>();
+			if(null!=mvarCurrentTopoStorage)
+			{
+				int storageId = mvarCurrentTopoStorage.Id;
+				List<DBAsimilation> entrada = await Asimilations.Where(x => x.TopoStorageId == storageId).ToListAsync();
+				foreach(DBAsimilation asimila in entrada)
+				{
+					Asimilation? elemento = await GetAsimilation(asimila.Id);
+					if (null != elemento)
+						salida.Add(elemento);
+				}
+			}
+			return salida;
+		}
 		#endregion Asimilation
 
 		#region Axis
@@ -312,7 +333,7 @@ namespace TimeNet2026.DBStorage
 		/// <returns>Eje recuperado</returns>
 		internal async Task<Axis?> getAxis(int AxisId)
 		{
-			if(null!=CurrentTopoStorage)
+			if(null!=mvarCurrentTopoStorage)
 			{
 				if (mcolAxisCache.ContainsKey(AxisId))
 					return mcolAxisCache[AxisId];
@@ -345,7 +366,7 @@ namespace TimeNet2026.DBStorage
 		internal async Task<List<Axis>> getAxis()
 		{
 			List<Axis> salida = new List<Axis>();
-			if(null!=CurrentTopoStorage)
+			if(null!=mvarCurrentTopoStorage)
 			{
 				int storageId = mvarCurrentTopoStorage.Id;
 				List<DBAxis> entrada = await Axis.Where(x => x.StorageId == storageId).ToListAsync();
