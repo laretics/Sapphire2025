bbeb459e840c7befeda90eaccace5e262c5b013b
ErPe <spamme@montefaro.eu>
2026-03-02 17:01:35 +0100

    Discriminador de tipos de archivos XML en servidor.

--- Sapphire2025Server/Controllers/SapphireTimeNetController.cs
+++ Sapphire2025Server/Controllers/SapphireTimeNetController.cs
@@ -6,6 +6,7 @@ using TimeNet2026.Storage;
 using TimeNet2026.Topo;
 using Sapphire2026.Data;
 using TimeNet2026Data;
+using System.Xml.Linq;
 
 namespace Sapphire2025Server.Controllers
 {
@@ -29,15 +30,45 @@ namespace Sapphire2025Server.Controllers
 		public async Task<XMLCompileResult> UploadXML([FromForm] IFormFile file)
 		{
 			XMLCompileResult salida = new XMLCompileResult();
-			
+			if(null==file || file.Length ==0)
+			{
+				salida.Success = false;
+				salida.Message = "No se ha recibido ningún archivo.";
+				return salida;
+			}
 
+			string xmlText;
+			using (StreamReader reader = new StreamReader(file.OpenReadStream()))
+			{
+				xmlText = await reader.ReadToEndAsync();
+			}
 
+			//Comprobación de que es un XML váludo.
+			try
+			{
+				XDocument xdoc = System.Xml.Linq.XDocument.Parse(xmlText);
+				XElement? root = xdoc.Root;
+				if(null==root)
+				{
+					salida.Success = false;
+					salida.Message = "El archivo no tiene un nodo raíz.";
+					return salida;
+				}
+				switch (root.Name.LocalName)
+				{
+					case "layout":
 
+					case "rautatie":
 
-
-			//Mensaje por defecto para probar la cadena de depuración.
-			salida.Success = false;
-			salida.Message = "Código vacío sin compilador";
+					default:
+						break;
+				}
+			}
+			catch (System.Xml.XmlException ex)
+			{
+				salida.Success = false;
+				salida.Message = string.Format("El arvhivo no es un XML válido: {0}", ex.Message);
+			}
 			return salida;
 		}
 
