547c83a14b8e0c9da6272a2e94ca58cc2a660da2
ErPe <spamme@montefaro.eu>
2026-03-22 20:11:02 +0100

    Bootstrap en la solución, carta de ajuste, parametrización de elementos, tren en cad para visualización en detalle, diferentes ventanas flotantes.

---
+++ Sapphire25/Tourmaline26/Components/Services/Logic/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace Tourmaline26.Components.Services.Logic
+{
+	public static class Enums
+	{
+		public enum TeslaMode //Afecta a la visualización de las ventanas y establece el modo de trabajo.
+		{
+			Iddle, //Navegador con todo apagado. Modo inicial.
+			DestinationSelect, //Selección de itinerario para la ruta.
+			Navigation, //Modo navegación. En la conducción normal del tren.
+			RightFrame //Panel derecho desplegado. Es el modo en el que se introducen datos.
+		}
+	}
+}
