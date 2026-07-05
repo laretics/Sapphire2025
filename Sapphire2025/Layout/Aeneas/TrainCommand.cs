da1c94b5abab27d3971b33fd4f3049334a4f391f
ErPe <spamme@montefaro.eu>
2026-07-05 13:33:58 +0200

    Remodelación de directorios de antiguos iconos svg. Cambios en barra de tareas de estados de tren en TrainDossier.

--- Sapphire2025/Layout/Aeneas/TrainCommand.cs
+++ Sapphire2025/Layout/Aeneas/TrainCommand.cs
@@ -2,14 +2,14 @@
 
 namespace Sapphire2025.Layout.Aeneas
 {
-	internal class TrainCommand
+	public class TrainCommand
 	{
 		public string Label { get; set; } = "";
 		public string Style { get; set; } = "outline-secondary";
 		public string CssClass { get => string.Format("btn btn-{0}",Style);}
-		public Func<bool> ShowIf { get; set; } = () => false; //Botón mostrado si ocurre algo.
-		public Func<bool> EnableIf { get; set; }  = () => false; //Botón habilitado si ocurre algo
+		public string IconId { get; set; } = string.Empty;
+		public string? IconColor{ get; set; }
+		public bool Enabled { get; set; } = true;
 		public Func<Task> Action { get; set; } = () => Task.CompletedTask;
-		public RenderFragment? Icon { get; set; }
 	}
 }
