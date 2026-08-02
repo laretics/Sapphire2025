398995e87854f619606c331642594e5bada2aaff
ErPe <spamme@montefaro.eu>
2026-08-02 21:32:54 +0200

    Versión 1.7 beta

--- Sapphire25/Tourmaline26/Logic/SystemConfiguration.cs
+++ Sapphire25/Tourmaline26/Logic/SystemConfiguration.cs
@@ -6,8 +6,8 @@ namespace Tourmaline26.Logic
 	/// </summary>
 	public class SystemConfiguration
 	{
-		public string Version  => "V1.6 (Beta)"; //Versión de este programa.
-		public DateTime Release => new DateTime(2026,8,1);
+		public string Version  => "V1.7 (Beta)"; //Versión de este programa.
+		public DateTime Release => new DateTime(2026,8,3);
 		public Guid TrainId { get; set; } //Guid de este material móvil según Zafiro.
         public string Name { get; set; } = "Tren";
 		public DateTime LastRelease { get; set; } //Fecha de la última instalación de hardware.
